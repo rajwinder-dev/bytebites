805c7bcc3e19ca63e178ad83c5f742e039024004
rajwindersxxx <rajwindersxxx@gmail.com>
2026-06-09 11:51:31 +0530

    basic push

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import { SecondaryButton } from "./_components/ui/Buttons";
 export default function error() {
   return (
     <div className="flex h-[91vh] flex-col items-center justify-center gap-8 text-center">
-      <h1 className="text-3xl font-semibold">Page Not Found !</h1>
+      <h1 className="text-3xl font-semibold">Page Not Found !, go back</h1>
       <Link href="/">
         <SecondaryButton className="bg-accent-500 text-primary-800 inline-block px-6 py-3 text-lg">
           Go back
